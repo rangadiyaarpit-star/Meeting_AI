309214096e98bacfd7ce4eab019c554982dac68b
Arpit <rangadiyaarpit@gmail.com>
2026-09-19 14:21:27 +0530

    Configure static root for Render

--- core/settings.py
+++ core/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'static/'
